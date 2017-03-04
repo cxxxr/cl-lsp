c3295906408e1377dddc9ee3206e082d882bf3d8
cxxxr <g23tlm@gmail.com>
2017-03-04 21:36:50 +0900

    remove unused function

--- editor.lisp
+++ editor.lisp
@@ -3,9 +3,7 @@
         :lem-base
         :cl-lsp/protocol)
   (:export :move-to-lsp-position
-           :make-lsp-position
            :make-lsp-range
-           :make-text-document-position
            :file-location
            :buffer-location
            :map-buffer-symbols))
@@ -19,11 +17,6 @@
     (line-offset point 0 |character|)
     point))
 
-(defun make-lsp-position (point)
-  (make-instance '|Position|
-                 :|line| (1- (line-number-at-point point))
-                 :|character| (point-charpos point)))
-
 (defun make-lsp-range (start end)
   (declare (type point start end))
   (let* ((start-line (1- (line-number-at-point start)))
@@ -33,13 +26,6 @@
     (make-instance '|Range|
                    :|start| (make-instance '|Position| :|line| start-line :|character| start-character)
                    :|end| (make-instance '|Position| :|line| end-line :|character| end-character))))
-
-(defun make-text-document-position (point)
-  (make-instance '|TextDocumentPositionParams|
-                 :|textDocument| (make-instance
-                                  '|TextDocumentIdentifier|
-                                  :|uri| (buffer-filename (point-buffer point)))
-                 :|position| (make-lsp-position point)))
 
 (defun line-location (file line start-charpos end-charpos)
   (make-instance
