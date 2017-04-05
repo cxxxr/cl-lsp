3f0bc4bc7ca6fdd16be92766d5e8baea5d1a4b1c
cxxxr <g23tlm@gmail.com>
2017-04-06 00:23:44 +0900

    fix indent-line

--- formatting.lisp
+++ formatting.lisp
@@ -11,28 +11,28 @@
 (in-package :cl-lsp/formatting)
 
 (defun indent-line (p editp)
-  (log-format "~D: ~A~%" (lem-base:line-number-at-point p) (lem-base:line-string p))
-  (let ((line-number (1- (lem-base:line-number-at-point p)))
-        (old-column (lem-base:point-column (lem-base:back-to-indentation p)))
-        (new-column (lem-lisp-syntax.indent:calc-indent p)))
-    (when new-column
-      (when editp
-        (lem-base:line-start p)
-        (lem-base:delete-character p old-column)
-        (lem-base:insert-character p #\space new-column))
-      (values
-       (convert-to-hash-table
-        (make-instance '|TextEdit|
-                       :|range| (make-instance
-                                 '|Range|
-                                 :|start| (make-instance '|Position|
+  (unless (lem-base:blank-line-p p)
+    (let ((line-number (1- (lem-base:line-number-at-point p)))
+          (old-column (lem-base:point-column (lem-base:back-to-indentation p)))
+          (new-column (lem-lisp-syntax.indent:calc-indent p)))
+      (when new-column
+        (when editp
+          (lem-base:line-start p)
+          (lem-base:delete-character p old-column)
+          (lem-base:insert-character p #\space new-column))
+        (values
+         (convert-to-hash-table
+          (make-instance '|TextEdit|
+                         :|range| (make-instance
+                                   '|Range|
+                                   :|start| (make-instance '|Position|
+                                                           :|line| line-number
+                                                           :|character| 0)
+                                   :|end| (make-instance '|Position|
                                                          :|line| line-number
-                                                         :|character| 0)
-                                 :|end| (make-instance '|Position|
-                                                       :|line| line-number
-                                                       :|character| old-column))
-                       :|newText| (make-string new-column :initial-element #\space)))
-       new-column))))
+                                                         :|character| old-column))
+                         :|newText| (make-string new-column :initial-element #\space)))
+         new-column)))))
 
 (defun on-type-formatting (point ch options)
   (declare (ignore ch options))
