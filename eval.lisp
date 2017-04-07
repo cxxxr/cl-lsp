fcd2c7bfbb6456144f944c08e157ebbd84092a0a
cxxxr <g23tlm@gmail.com>
2017-04-07 22:52:30 +0900

    avoid dead lock

--- eval.lisp
+++ eval.lisp
@@ -50,46 +50,47 @@
 
 
 (defun compilation-notes-to-diagnostics (notes)
-  (let ((diagnostics '()))
-    (dolist (note notes)
-      (optima:match note
-        ((and (optima:property :location
-                               (or (list :location
-                                         (list :buffer buffer-name)
-                                         (list :offset pos _)
-                                         _)
-                                   (list :location
-                                         (list :file file)
-                                         (list :position pos)
-                                         _)))
-              (or (optima:property :message message) (and))
-              (or (optima:property :severity severity) (and))
-              (or (optima:property :source-context _source-context) (and)))
-         (let* ((buffer (if buffer-name
-                            (lem-base:get-buffer buffer-name)
-                            (lem-base:get-file-buffer file)))
-                (point (lem-base:buffer-point buffer)))
-           (lem-base:move-to-position point pos)
-           (lem-base:skip-chars-backward point #'lem-base:syntax-symbol-char-p)
-           (lem-base:with-point ((end point))
-             (unless (lem-base:form-offset end 1)
-               (when (eq severity :read-error)
-                 (lem-base:buffer-start point))
-               (lem-base:buffer-end end))
-             (push (make-instance '|Diagnostic|
-                                  :|range| (make-lsp-range point end)
-                                  :|severity| (case severity
-                                                ((:error :read-error)
-                                                 |DiagnosticSeverity.Error|)
-                                                ((:warning :style-warning)
-                                                 |DiagnosticSeverity.Warning|)
-                                                ((:note :redefinition)
-                                                 |DiagnosticSeverity.Information|))
-                                  ;; :|code|
-                                  ;; :|source|
-                                  :|message| message)
-                   diagnostics))))))
-    (list-to-object[] diagnostics)))
+  (bt:with-lock-held (*method-lock*)
+    (let ((diagnostics '()))
+      (dolist (note notes)
+        (optima:match note
+          ((and (optima:property :location
+                                 (or (list :location
+                                           (list :buffer buffer-name)
+                                           (list :offset pos _)
+                                           _)
+                                     (list :location
+                                           (list :file file)
+                                           (list :position pos)
+                                           _)))
+                (or (optima:property :message message) (and))
+                (or (optima:property :severity severity) (and))
+                (or (optima:property :source-context _source-context) (and)))
+           (let* ((buffer (if buffer-name
+                              (lem-base:get-buffer buffer-name)
+                              (lem-base:get-file-buffer file)))
+                  (point (lem-base:buffer-point buffer)))
+             (lem-base:move-to-position point pos)
+             (lem-base:skip-chars-backward point #'lem-base:syntax-symbol-char-p)
+             (lem-base:with-point ((end point))
+               (unless (lem-base:form-offset end 1)
+                 (when (eq severity :read-error)
+                   (lem-base:buffer-start point))
+                 (lem-base:buffer-end end))
+               (push (make-instance '|Diagnostic|
+                                    :|range| (make-lsp-range point end)
+                                    :|severity| (case severity
+                                                  ((:error :read-error)
+                                                   |DiagnosticSeverity.Error|)
+                                                  ((:warning :style-warning)
+                                                   |DiagnosticSeverity.Warning|)
+                                                  ((:note :redefinition)
+                                                   |DiagnosticSeverity.Information|))
+                                    ;; :|code|
+                                    ;; :|source|
+                                    :|message| message)
+                     diagnostics))))))
+      (list-to-object[] diagnostics))))
 
 (defun compilation-message (notes secs successp)
   (with-output-to-string (out)
@@ -106,37 +107,35 @@
 (defun compile-and-load-file (uri)
   (let ((filename (uri-to-filename uri))
         result)
-    (bt:with-lock-held (*method-lock*)
-      (handler-case (with-output-to-string (*standard-output*)
-                      (setf result (swank-compile-file filename t)))
-        (error (c)
-          (notify-show-message |MessageType.Error|
-                               (princ-to-string c))
-          (setf result nil))))
+    (handler-case (with-output-to-string (*standard-output*)
+                    (setf result (swank-compile-file filename t)))
+      (error (c)
+        (notify-show-message |MessageType.Error|
+                             (princ-to-string c))
+        (setf result nil)))
     (when result
       (destructuring-bind (notes successp duration loadp fastfile)
           (rest result)
-        (bt:with-lock-held (*method-lock*)
-          (notify-show-message |MessageType.Info|
-                               (compilation-message
-                                notes duration successp))
-          (let ((diagnostics (compilation-notes-to-diagnostics notes)))
-            (let ((diagnostics-params
-                   (convert-to-hash-table
-                    (make-instance '|PublishDiagnosticsParams|
-                                   :|uri| uri
-                                   :|diagnostics| diagnostics))))
-              (jsonrpc:notify-async *server*
-                                    "textDocument/publishDiagnostics"
-                                    diagnostics-params)
-              (when (and loadp fastfile successp)
-                (handler-case (let ((output-string
-                                     (with-output-to-string (*standard-output*)
-                                       (load fastfile))))
-                                (notify-log-message |MessageType.Log| output-string))
-                  (error (condition)
-                    (notify-show-message |MessageType.Error|
-                                         (princ-to-string condition))))))))))))
+        (notify-show-message |MessageType.Info|
+                             (compilation-message
+                              notes duration successp))
+        (let ((diagnostics (compilation-notes-to-diagnostics notes)))
+          (let ((diagnostics-params
+                 (convert-to-hash-table
+                  (make-instance '|PublishDiagnosticsParams|
+                                 :|uri| uri
+                                 :|diagnostics| diagnostics))))
+            (jsonrpc:notify-async *server*
+                                  "textDocument/publishDiagnostics"
+                                  diagnostics-params)
+            (when (and loadp fastfile successp)
+              (handler-case (let ((output-string
+                                   (with-output-to-string (*standard-output*)
+                                     (load fastfile))))
+                              (notify-log-message |MessageType.Log| output-string))
+                (error (condition)
+                  (notify-show-message |MessageType.Error|
+                                       (princ-to-string condition)))))))))))
 
 (define-method "lisp/compileAndLoadFile" (params |TextDocumentIdentifier|)
   (let* ((uri (slot-value params '|uri|)))
