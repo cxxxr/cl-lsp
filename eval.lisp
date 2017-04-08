26e9a1fecb8ed5fc7bc0337570d09c538cf545ae
cxxxr <g23tlm@gmail.com>
2017-04-08 18:27:38 +0900

    lock

--- eval.lisp
+++ eval.lisp
@@ -52,51 +52,85 @@
   (jsonrpc:notify-async *server* "lisp/evalBegin" nil)
   (send (lambda ()
           (funcall function)
-          (jsonrpc:notify-async *server* "lisp/evalEnd" nil))))
+          (bt:with-lock-held (*method-lock*)
+            (jsonrpc:notify-async *server* "lisp/evalEnd" nil)))))
+
+(defun lsp-output-fn (string)
+  (bt:with-lock-held (*method-lock*)
+    (notify-log-message |MessageType.Log| string)))
+
+(defun call-with-eval-stream (function)
+  (let ((out (make-instance 'lsp-output-stream :output-fn #'lsp-output-fn)))
+    (with-input-from-string (in "")
+      (with-open-stream (eval-stream (make-two-way-stream in out))
+        (let ((*standard-output* eval-stream)
+              (*error-output* eval-stream)
+              (*standard-input* eval-stream)
+              (*terminal-io* eval-stream)
+              (*query-io* eval-stream)
+              (*debug-io* eval-stream)
+              (*trace-output* eval-stream))
+          (funcall function eval-stream))))))
+
+(defmacro with-eval-stream ((stream-var) &body body)
+  `(call-with-eval-stream (lambda (,stream-var) ,@body)))
+
+(defun call-with-muffle-streams (function)
+  (let ((stream (make-broadcast-stream)))
+    (let ((*standard-output* stream)
+          (*error-output* stream)
+          (*standard-input* stream)
+          (*terminal-io* stream)
+          (*query-io* stream)
+          (*debug-io* stream)
+          (*trace-output* stream))
+      (funcall function))))
+
+(defmacro with-muffle-streams (() &body body)
+  `(call-with-muffle-streams (lambda () ,@body)))
 
 
 (defun compilation-notes-to-diagnostics (notes)
-  (bt:with-lock-held (*method-lock*)
-    (let ((diagnostics '()))
-      (dolist (note notes)
-        (optima:match note
-          ((and (optima:property :location
-                                 (or (list :location
-                                           (list :buffer buffer-name)
-                                           (list :offset pos _)
-                                           _)
-                                     (list :location
-                                           (list :file file)
-                                           (list :position pos)
-                                           _)))
-                (or (optima:property :message message) (and))
-                (or (optima:property :severity severity) (and))
-                (or (optima:property :source-context _source-context) (and)))
-           (let* ((buffer (if buffer-name
-                              (lem-base:get-buffer buffer-name)
-                              (lem-base:get-file-buffer file)))
-                  (point (lem-base:buffer-point buffer)))
-             (lem-base:move-to-position point pos)
-             (lem-base:skip-chars-backward point #'lem-base:syntax-symbol-char-p)
-             (lem-base:with-point ((end point))
-               (unless (lem-base:form-offset end 1)
-                 (when (eq severity :read-error)
-                   (lem-base:buffer-start point))
-                 (lem-base:buffer-end end))
-               (push (make-instance '|Diagnostic|
-                                    :|range| (make-lsp-range point end)
-                                    :|severity| (case severity
-                                                  ((:error :read-error)
-                                                   |DiagnosticSeverity.Error|)
-                                                  ((:warning :style-warning)
-                                                   |DiagnosticSeverity.Warning|)
-                                                  ((:note :redefinition)
-                                                   |DiagnosticSeverity.Information|))
-                                    ;; :|code|
-                                    ;; :|source|
-                                    :|message| message)
-                     diagnostics))))))
-      (list-to-object[] diagnostics))))
+  (let ((diagnostics '()))
+    (dolist (note notes)
+      (optima:match note
+        ((and (optima:property :location
+                               (or (list :location
+                                         (list :buffer buffer-name)
+                                         (list :offset pos _)
+                                         _)
+                                   (list :location
+                                         (list :file file)
+                                         (list :position pos)
+                                         _)))
+              (or (optima:property :message message) (and))
+              (or (optima:property :severity severity) (and))
+              (or (optima:property :source-context _source-context) (and)))
+         (let* ((buffer (if buffer-name
+                            (lem-base:get-buffer buffer-name)
+                            (lem-base:get-file-buffer file)))
+                (point (lem-base:buffer-point buffer)))
+           (lem-base:move-to-position point pos)
+           (lem-base:skip-chars-backward point #'lem-base:syntax-symbol-char-p)
+           (lem-base:with-point ((end point))
+             (unless (lem-base:form-offset end 1)
+               (when (eq severity :read-error)
+                 (lem-base:buffer-start point))
+               (lem-base:buffer-end end))
+             (push (make-instance '|Diagnostic|
+                                  :|range| (make-lsp-range point end)
+                                  :|severity| (case severity
+                                                ((:error :read-error)
+                                                 |DiagnosticSeverity.Error|)
+                                                ((:warning :style-warning)
+                                                 |DiagnosticSeverity.Warning|)
+                                                ((:note :redefinition)
+                                                 |DiagnosticSeverity.Information|))
+                                  ;; :|code|
+                                  ;; :|source|
+                                  :|message| message)
+                   diagnostics))))))
+    (list-to-object[] diagnostics)))
 
 (defun compilation-message (notes secs successp)
   (with-output-to-string (out)
@@ -113,72 +147,63 @@
 (defun compile-and-load-file (uri)
   (let ((filename (uri-to-filename uri))
         result)
-    (handler-case (with-output-to-string (*standard-output*)
+    (handler-case (with-muffle-streams ()
                     (setf result (swank-compile-file filename t)))
       (error (c)
-        (notify-show-message |MessageType.Error|
-                             (princ-to-string c))
+        (bt:with-lock-held (*method-lock*)
+          (notify-show-message |MessageType.Error|
+                               (princ-to-string c)))
         (setf result nil)))
     (when result
       (destructuring-bind (notes successp duration loadp fastfile)
           (rest result)
-        (notify-show-message |MessageType.Info|
-                             (compilation-message
-                              notes duration successp))
-        (let ((diagnostics (compilation-notes-to-diagnostics notes)))
-          (let ((diagnostics-params
-                 (convert-to-hash-table
-                  (make-instance '|PublishDiagnosticsParams|
-                                 :|uri| uri
-                                 :|diagnostics| diagnostics))))
-            (jsonrpc:notify-async *server*
-                                  "textDocument/publishDiagnostics"
-                                  diagnostics-params)
-            (when (and loadp fastfile successp)
-              (handler-case (let ((output-string
-                                   (with-output-to-string (*standard-output*)
-                                     (load fastfile))))
-                              (notify-log-message |MessageType.Log| output-string))
-                (error (condition)
-                  (notify-show-message |MessageType.Error|
-                                       (princ-to-string condition)))))))))))
+        (bt:with-lock-held (*method-lock*)
+          (notify-show-message |MessageType.Info|
+                               (compilation-message
+                                notes duration successp))
+          (jsonrpc:notify-async
+           *server*
+           "textDocument/publishDiagnostics"
+           (convert-to-hash-table
+            (make-instance '|PublishDiagnosticsParams|
+                           :|uri| uri
+                           :|diagnostics| (compilation-notes-to-diagnostics notes)))))
+        (when (and loadp fastfile successp)
+          (handler-case
+              (with-eval-stream (eval-stream)
+                (load fastfile)
+                (finish-output eval-stream))
+            (error (condition)
+              (bt:with-lock-held (*method-lock*)
+                (notify-show-message |MessageType.Error|
+                                     (princ-to-string condition))))))))))
 
 (define-method "lisp/compileAndLoadFile" (params |TextDocumentIdentifier|)
   (let* ((uri (slot-value params '|uri|)))
     (send-eval (lambda () (compile-and-load-file uri))))
   nil)
 
-(defun lsp-output-fn (string)
-  (notify-log-message |MessageType.Log| string))
-
 (defun eval-string (string package)
   (let ((*package* (ensure-package package))
         results)
-    (let ((_out (make-instance 'lsp-output-stream :output-fn #'lsp-output-fn)))
-      (with-input-from-string (_in "")
-        (with-open-stream (eval-stream (make-two-way-stream _in _out))
-          (let ((*standard-output* eval-stream)
-                (*error-output* eval-stream)
-                (*standard-input* eval-stream)
-                (*terminal-io* eval-stream)
-                (*query-io* eval-stream)
-                (*debug-io* eval-stream)
-                (*trace-output* eval-stream))
-            (handler-bind
-                ((error (lambda (err)
-                          (finish-output eval-stream)
-                          (notify-log-message |MessageType.Error|
-                                              (with-output-to-string (out)
-                                                (format out "~%~A~%~%" err)
-                                                (uiop:print-backtrace :stream out)))
-                          (notify-show-message |MessageType.Error|
-                                               (princ-to-string err))
-                          (return-from eval-string))))
-              (setf results
-                    (multiple-value-list
-                     (eval (read-from-string string))))))
-          (finish-output eval-stream)
-          (notify-show-message |MessageType.Info| (format nil "~{~A~^, ~}" results)))))))
+    (with-eval-stream (eval-stream)
+      (handler-bind
+          ((error (lambda (err)
+                    (finish-output eval-stream)
+                    (bt:with-lock-held (*method-lock*)
+                      (notify-log-message |MessageType.Error|
+                                          (with-output-to-string (out)
+                                            (format out "~%~A~%~%" err)
+                                            (uiop:print-backtrace :stream out)))
+                      (notify-show-message |MessageType.Error|
+                                           (princ-to-string err)))
+                    (return-from eval-string))))
+        (setf results
+              (multiple-value-list
+               (eval (read-from-string string)))))
+      (finish-output eval-stream)
+      (bt:with-lock-held (*method-lock*)
+        (notify-show-message |MessageType.Info| (format nil "~{~A~^, ~}" results))))))
 
 (defun send-eval-string (string package)
   (send-eval (lambda () (eval-string string package))))
