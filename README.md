9621447bf8f427ac59cfe7a08d84d9c7245e2c54
Angel Cortez <acort3255@gmail.com>
2020-05-31 22:07:07 -0400

    • Update for support in Visual Studio Code

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
-# cl-lsp
-An implementation of the Language Server Protocol for Common Lisp
+# CL-LSP
+CL-LSP is an implementation of the [Language Server Protocol](https://microsoft.github.io/language-server-protocol/) (LSP) for Common Lisp.
 
-# Installation
-* Install [roswell](https://github.com/roswell/roswell/)
-* `$ ros install cxxxr/cl-lsp`
+## Getting Started
 
-# VS Code
+The CL-LSP server is included with the Swift toolchain. Depending on how you installed Swift, you may already have SourceKit-LSP. Make sure you build your package with the same toolchain as you use sourcekit-lsp from to ensure compatibility.
 
-depends on [vscode-lisp](https://github.com/mattn/vscode-lisp)
+1. Get CL-LSP Installed
+
+    1. Install [roswell](https://github.com/roswell/roswell/)
+
+    2. Then install `CL-LSP`: `$ ros install cxxxr/cl-lsp`
+
+    3. Verfiy installation: `~/.roswell/bin/cl-lsp`.
+
+2. Configure your editor to use CL-LSP. See [Editors](Editors) for more information about editor integration.
+
+3. Enjoy!
+
+# Visual Studio Code
+
+Depends `CL-LSP` on [vscode-lisp](https://github.com/mattn/vscode-lisp). See [vscode](vscode) for more information about Visual Studio Code integration.
 
 ```
-$ cd ~/.roswell/cl-lsp/vscode
-$ npm install
-$ code .
+$ cd vscode
+$ npm run createDevPackage
+$ code --install-extension out/cl-lsp-vscode-dev.vsix
 ```
--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -4,15 +4,11 @@
  * ------------------------------------------------------------------------------------------ */
 'use strict';
 
-import * as path from 'path';
-import * as child_process from 'child_process';
 import {
-	workspace, Disposable, ExtensionContext, languages,
-	window, commands, InputBoxOptions
+	workspace, ExtensionContext, window, commands
 } from 'vscode';
 import {
-	LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions,
-	TransportKind, TextDocumentIdentifier, TextDocumentPositionParams
+	LanguageClient, LanguageClientOptions, ServerOptions, TextDocumentIdentifier, TextDocumentPositionParams, Executable
 } from 'vscode-languageclient';
 
 let languageClient: LanguageClient;
@@ -21,7 +17,7 @@ function startRepl() {
 }
 
 function getTextDocumentIdentifier() {
-	let document = window.activeTextEditor.document;
+	let document = window.activeTextEditor!.document;
 	let params: TextDocumentIdentifier = {
 		uri: document.uri.toString()
 	}
@@ -30,66 +26,73 @@ function getTextDocumentIdentifier() {
 
 function compileAndLoadFile() {
 	let params = getTextDocumentIdentifier();
-	languageClient.sendNotification("lisp/compileAndLoadFile", params);
+	languageClient.sendNotification('lisp/compileAndLoadFile', params);
 }
 
 function evaluate() {
-	let selection = window.activeTextEditor.selection;
+	let selection = window.activeTextEditor!.selection;
 	if (selection.isEmpty) {
 		let params: TextDocumentPositionParams = {
 			textDocument: getTextDocumentIdentifier(),
 			position: selection.active
 		}
-		languageClient.sendNotification("lisp/eval", params);
+		languageClient.sendNotification('lisp/eval', params);
 	} else {
 		let params = {
 			textDocument: getTextDocumentIdentifier(),
 			range: selection
 		}
-		languageClient.sendNotification("lisp/rangeEval", params);
+		languageClient.sendNotification('lisp/rangeEval', params);
 	}
 }
 
 function interrupt() {
-	languageClient.sendNotification("lisp/interrupt", {});
+	languageClient.sendNotification('lisp/interrupt', {});
 }
 
 function indentLine() {
 }
 
 export function activate(context: ExtensionContext) {
-	let serverOptions: ServerOptions;
 
-	serverOptions = () => new Promise<child_process.ChildProcess>((resolve, reject) => {
-		function spawnServer(...args: string[]): child_process.ChildProcess {
-			let childProcess = child_process.spawn("cl-lsp", ["stdio"]);
-			return childProcess;
-		}
-		resolve(spawnServer());
-	});
+	const config = workspace.getConfiguration('cl-lsp');
+
+	const cl: Executable = {
+        command: config.get<string>('serverPath', 'cl-lsp'),
+        args: config.get<string[]>('serverArguments', [])
+	};
+	
+	const toolchain = config.get<string>('toolchainPath', '');
+    if (toolchain) {
+        cl.options = { env: { ...process.env, CL_LSP_TOOLCHAIN_PATH: toolchain } };
+	}
+	
+	const serverOptions: ServerOptions = cl;
 
 	let clientOptions: LanguageClientOptions = {
-		documentSelector: ["lisp"],
-		synchronize: {
-			configurationSection: 'languageServerExample'
-		}
+		documentSelector: [
+			'lisp'
+		],
+		synchronize: undefined 
 	}
 
-	languageClient = new LanguageClient("Common Lisp Language Server", serverOptions, clientOptions);
+	languageClient = new LanguageClient('cl-lsp', 'Common Lisp Language Server', serverOptions, clientOptions);
 	languageClient.onReady().then(function (x) {
-		languageClient.onNotification("lisp/evalBegin", function (f) {
-			window.setStatusBarMessage("Eval...");
+		languageClient.onNotification('lisp/evalBegin', function (f) {
+			window.setStatusBarMessage('Eval...');
 		})
-		languageClient.onNotification("lisp/evalEnd", function (f) {
-			window.setStatusBarMessage("Done");
+		languageClient.onNotification('lisp/evalEnd', function (f) {
+			window.setStatusBarMessage('Done');
 		})
 	})
 
 	context.subscriptions.push(languageClient.start());
 
-	context.subscriptions.push(commands.registerCommand("lisp.indentLine", () => indentLine()));
-	context.subscriptions.push(commands.registerCommand("lisp.compileAndLoadFile", () => compileAndLoadFile()));
-	context.subscriptions.push(commands.registerCommand("lisp.eval", () => evaluate()));
-	context.subscriptions.push(commands.registerCommand("lisp.interrupt", () => interrupt()));
-	context.subscriptions.push(commands.registerCommand("lisp.replStart", () => startRepl()));
+	context.subscriptions.push(commands.registerCommand('lisp.indentLine', () => indentLine()));
+	context.subscriptions.push(commands.registerCommand('lisp.compileAndLoadFile', () => compileAndLoadFile()));
+	context.subscriptions.push(commands.registerCommand('lisp.eval', () => evaluate()));
+	context.subscriptions.push(commands.registerCommand('lisp.interrupt', () => interrupt()));
+	context.subscriptions.push(commands.registerCommand('lisp.replStart', () => startRepl()));
+
+	console.log('CL-LSP is now active!');
 }
