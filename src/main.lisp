4b2b6a234c85d52a3e6788f1a19daec7efd6acb5
cxxxr <g23tlm@gmail.com>
2023-02-02 23:23:08 +0900

    fix

--- src/main.lisp
+++ src/main.lisp
@@ -14,6 +14,7 @@
                  (start-tcp-server (parse-integer port))
                  (start-tcp-server 10003))))
           ((equal mode "stdio")
+           (log:config :off)
            (start-stdio-server))
           (t
            (uiop:println (format nil "unknown mode: ~A" mode))
